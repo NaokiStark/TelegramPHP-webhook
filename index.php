aa9d1746c326ad58b3dab8691bdfac8e3681c97b
Fabián <max_fabian@hotmail.com.ar>
2015-06-25 06:38:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,3 +22,5 @@ if(strrpos($text,'/ping')!==false){
   //Send request with api
   $req->postRequest('https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/sendMessage',$parms);
 }
+
+?>
